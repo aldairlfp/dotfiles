2c6db900ca1b8ff1523c63b4f881d5b8caba969f
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2023-10-21 01:52:57 -0400

    Last update from nvim

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -24,5 +24,43 @@ local plugins = {
       require "custom.configs.lspconfig"
     end,
   },
+  {
+    "zbirenbaum/copilot.lua",
+    -- Lazy load when event occurs. Events are triggered
+    -- as mentioned in:
+    -- https://vi.stackexchange.com/a/4495/20389
+    event = "InsertEnter",
+    -- You can also have it load at immediately at
+    -- startup by commenting above and uncommenting below:
+    -- lazy = false
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    debounce = 75,
+    keymap = {
+      accept = "<M-l>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<C-]>",
+    },
+  },
+  filetypes = {
+    yaml = false,
+    markdown = false,
+    help = false,
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+  },
+      })
+    end,
+  },
 }
 return plugins
